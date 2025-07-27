c03f3ae59ebeb62cd6d1cace52f151564f7e053c
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2025-07-27 19:35:39 +0700

    Create data-produk.js
    data produk

---
+++ js/data-produk.js
@@ -0,0 +1,69 @@
+const produkKnowledge = {
+  "Server": [
+    {
+      nama: "DELL PowerEdge R640",
+      gambar: "https://images.tokopedia.net/img/cache/200-square/o3syd0/1997/1/1/c7ca9f6ba9c74273970ed04df868f916~.jpeg",
+      deskripsi: "Server rack 1U 8 Bays 2.5"
+    },
+    {
+      nama: "Dell PowerEdge R730",
+      gambar: "https://images.tokopedia.net/img/cache/200-square/o3syd0/1997/1/1/c31cba34791d41eca165988a18dfa150~.jpeg",
+      deskripsi: "Server rack 2U 8 Bays 3.5"
+    },
+    {
+      nama: "Dell PowerEdge R630",
+      gambar: "https://gugik.id/wp-content/uploads/2025/07/ginee_20250605164030092_3554312546-1-600x600.jpeg",
+      deskripsi: "Server rack 1U 10 Bays 2,5"
+    }
+  ],
+  "Switch": [
+    {
+      nama: "Cisco Nexus 3164Q, 64 QSFP+ ports, 2RU switch (N3K-C3164Q-40GE)",
+      gambar: "https://images.tokopedia.net/img/cache/200-square/o3syd0/1997/1/1/9952a91429cd4ccabc7438c34d4f1b2f~.jpeg",
+      deskripsi: "Switch Cisco 64 port"
+    },
+    {
+      nama: "Cisco Nexus 9348GC-FXP Switch N9K-C9348GC-FXP",
+      gambar: "https://images.tokopedia.net/img/cache/500-square/o3syd0/1997/1/1/5fdd20474eab442d962176d250af9d25~.jpeg.webp?ect=4g",
+      deskripsi: "Switch Ports: 48 x 1-GBASE-T ports, 4 x 1/10/25-Gbps SFP28 ports and 2 x 40/100 QSFP28 ports"
+    },
+  ],
+  "Komponen Server": [
+    {
+      nama: "RAM HPE 16GB 2Rx8 PC4-3200AA-R Smart Memory Kit",
+      gambar: "https://images.tokopedia.net/img/cache/200-square/o3syd0/1997/1/1/87792ddcc2e147b0be645522736d75a9~.jpeg",
+      deskripsi: "RAM HPE 16GB Smart Memory Kit."
+    },
+    {
+      nama: "Dell EMC SSD 2.5” SATA 6 Gbps 480GB – D3-S4610 SERIES",
+      gambar: "https://gugik.id/wp-content/uploads/2025/06/ginee_20250604154316476_4074611621-300x300.jpeg",
+      deskripsi: "SSD server 480GB"
+    },
+    {
+      nama: "iDRAC 9 Enterprise License For Dell 14th GEN Server R740xd R540 R440 R340 R240",
+      gambar: "https://gugik.id/wp-content/uploads/2024/11/423d2183-e585-4cb0-b9f4-d96646e0f2f7-600x600.jpg",
+      deskripsi: "License untuk IDRAC 9 Enterprise."
+    },
+  
+  ],
+  "NIC":  [
+    {
+      nama: "Ethernet Server Adapter Broadcom BCM57810-SFP+ PCIe 10G Dual-Port SFP+",
+      gambar: "https://images.tokopedia.net/img/cache/500-square/o3syd0/1997/1/1/4a9be4b0988d415aae69b52f59f74132~.jpeg.webp?ect=4g",
+      deskripsi: "SFP PCIe 10G"
+    }
+
+  ],
+  "Acces Point": [
+    {
+      nama: "MIKROTIK Wireless Indoor RBcAPGi-5acD2nD (cAP ac)",
+      gambar: "https://images.tokopedia.net/img/cache/500-square/o3syd0/1997/1/1/5072e740352d43c8af35141bf4f7137c~.jpeg.webp?ect=4g",
+      deskripsi: "RBcAPGi-5acD2nD adalah type tertinggi dari seri cAP dan merupakan solusi Wireless AP untuk Jaringan Hotspot."
+    },
+    {
+      nama: "",
+      gambar: "https://images.tokopedia.net/img/cache/500-square/o3syd0/1997/1/1/1d077ea1b06b4a24945df3392a20a893~.jpeg.webp?ect=4g",
+      deskripsi: "Access point WiFi 5 dengan manajemen terpusat. Cocok untuk jaringan bisnis kecil."
+    }
+  ]
+};
