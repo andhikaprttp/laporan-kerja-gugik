22d26ddc88e2c58d5d21a2904e998a3b1bc384ca
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2025-07-27 19:36:32 +0700

    Create data-absensi.js

---
+++ js/data-absensi.js
@@ -0,0 +1,36 @@
+const absensiData = {
+  "2025-06": [
+    { tanggal: '2 Juni 2025', masuk: '08:04', pulang: '18:41', lembur: '-' },
+    { tanggal: '3 Juni 2025', masuk: '08:06', pulang: '17:19', lembur: '-' },
+    { tanggal: '4 Juni 2025', masuk: '08:04', pulang: '17:22', lembur: '-' },
+    { tanggal: '5 Juni 2025', masuk: '08:07', pulang: '21.04', lembur: '-' },
+    { tanggal: '7 Juni 2025', masuk: '07:57', pulang: '18.01', lembur: '-' },
+    { tanggal: '9 Juni 2025', masuk: '07:56', pulang: '18:20', lembur: '-' },
+    { tanggal: '11 Juni 2025', masuk: '07:59', pulang: '18.42', lembur: '-' },
+    { tanggal: '12 Juni 2025', masuk: '08:08', pulang: '21:03', lembur: '-' },
+    { tanggal: '13 Juni 2025', masuk: '08:06', pulang: '17:31', lembur: '-' },
+    { tanggal: '14 Juni 2025', masuk: '08:14', pulang: '17:02', lembur: '-' },
+    { tanggal: '16 Juni 2025', masuk: '07:56', pulang: '17:23', lembur: '-' },
+    { tanggal: '17 Juni 2025', masuk: '07:57', pulang: '18:36', lembur: '-' },
+    { tanggal: '19 Juni 2025', masuk: '08:08', pulang: '18:01', lembur: '-' },
+    { tanggal: '20 Juni 2025', masuk: '07:56', pulang: '21:02', lembur: '-' }, 
+    { tanggal: '21 Juni 2025', masuk: '08:04', pulang: '17:00', lembur: '-' },
+    { tanggal: '22 Juni 2025', masuk: '08:05', pulang: '17:15', lembur: '-' },
+    { tanggal: '23 Juni 2025', masuk: '08:10', pulang: '17:05', lembur: '-' },
+    { tanggal: '24 Juni 2025', masuk: '08:00', pulang: '17:00', lembur: '-' },
+    { tanggal: '25 Juni 2025', masuk: '08:00', pulang: '17:00', lembur: '-' },
+    { tanggal: '26 Juni 2025', masuk: '08:00', pulang: '16:30', lembur: '-' },
+    { tanggal: '27 Juni 2025', masuk: '08:15', pulang: '17:20', lembur: '-' },
+    { tanggal: '28 Juni 2025', masuk: '08:00', pulang: '17:00', lembur: '-' },
+    { tanggal: '29 Juni 2025', masuk: '08:05', pulang: '17:10', lembur: '-' },
+    { tanggal: '30 Juni 2025', masuk: '08:00', pulang: '17:00', lembur: '-' }
+  ],
+  "2025-07": [
+    { tanggal: '1 Juli 2025', masuk: '08:00', pulang: '17:00', lembur: '-' },
+    { tanggal: '2 Juli 2025', masuk: '08:05', pulang: '17:10', lembur: '-' },
+
+
+
+    // Add more attendance data as needed
+  ]
+};
